cbeef9614da4b0793c64a7b7b0f7dc54cd9aab22
MarianeRendon <msderam@gmail.com>
2025-01-15 16:40:11 -0400

    Practica 1 commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import Navigator from './Components/Navigator/Navigator';
+import Router from './Components/Navigator/Router';
+import Product from './screens/Product';
+
+function App() {
+  return (
+    <>
+      <Product />
+      <Navigator />
+      <Router />
+    </>
+  )
+}
+
+export default App
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/counter/counterSlice.js'
+import productReducer from './features/product/productSlice.js';
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    product: productReducer,
+  },
+})
+export default store
