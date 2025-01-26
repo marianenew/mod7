a9d8e753a68725f5d5f8885641df51b8a922fee4
MarianeRendon <msderam@gmail.com>
2025-01-26 11:22:00 -0400

    Proyecto Modulo 07

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter basename="/mod7/">
+          <BrowserRouter basename="/modulo07/">
             <App />
           </BrowserRouter>
       </Provider>
